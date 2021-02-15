018cecc09e1e5dee8eb2570a3b32184b44c6fc97
Janesummers <1752321720@qq.com>
2021-02-15 08:54:55 +0800

    数据库配置同步

--- util/mysqlOpt.js
+++ util/mysqlOpt.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 let pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: 'root',
+  password: 'janesummer1175',
   port: '3306',
   database: 'cms'
 });
